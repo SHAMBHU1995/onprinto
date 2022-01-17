34910b2faf2b4422df4303f26e0e7853905bda80
SHAMBHU1995 <shambhusinghdce@gmail.com>
2022-01-17 20:15:14 +0530

    register module

--- app/code/Api/Custom/registration.php
+++ app/code/Api/Custom/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Api_Module',
+    'Api_Custom',
     __DIR__
 );
