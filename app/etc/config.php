409134bfe0c820f25821eb5e3b87efd71c3fcc01
SHAMBHU1995 <shambhusinghdce@gmail.com>
2022-01-22 11:26:55 +0530

    enable module

--- app/etc/config.php
+++ app/etc/config.php
@@ -301,7 +301,7 @@ return [
         'Amazon_Core' => 1,
         'Amazon_Login' => 1,
         'Amazon_Payment' => 1,
-        'Api_Module' => 1,
+        'Api_Custom' => 1,
         'Dotdigitalgroup_Email' => 1,
         'Dotdigitalgroup_Chat' => 1,
         'Klarna_Core' => 1,
