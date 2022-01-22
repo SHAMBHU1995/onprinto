db48df6386d16a3fae3ed14b64e2f91a2180d1ae
SHAMBHU1995 <shambhusinghdce@gmail.com>
2022-01-22 12:21:19 +0530

    remove unwated api

--- app/code/Api/Custom/Model/Category.php
+++ app/code/Api/Custom/Model/Category.php
@@ -22,46 +22,4 @@ class Category  implements \Api\Custom\Api\CategoryInterface
         echo json_encode($data);die;
     }
 
-    public function product()
-    {
-        $json_str = file_get_contents('php://input');
-        $json_obj = json_decode($json_str,true);
-    
-        $token = isset($json_obj['token'])?$json_obj['token']:null;
-        $cat = isset($json_obj['cat'])?$json_obj['cat']:null;
-
-        if(!empty($token)){
-            $tokenCollection = $this->helperData->authenticateToken($token);
-            if(count($tokenCollection) == 0){
-                $code = 0;
-                $message = "Invalid Api token";
-                $data =  array('code' => $code , 'message' => $message );
-                echo json_encode($data);die;
-      
-              }
-          }else{
-            $code = 0;
-            $message = "Access token missing";
-            $data =  array('code' => $code , 'message' => $message );
-            echo json_encode($data);die;
-          }
-          if(empty($cat)){
-            $code = 0;
-            $message = "Invalid parameter";
-            $data =  array('code' => $code , 'message' => $message );
-            echo json_encode($data);die;
-          }
-          $result = [];
-          if($cat){
-            $collection = $this->helperData->getCategoryProducts($cat);
-            
-            foreach($collection as $product){
-                $result[] = $product->getData();
-            }
-            $code = 1;
-            $data =  array('code' => $code , 'product' => $result );
-            echo json_encode($data);die;
-        }
-            
-    }
 }
